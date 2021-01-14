a196e7e7395f662e077317588c0e442413021fe4
Sean <seanskg72@gmail.com>
2021-01-14 16:35:17 +1100

    changed grounded output

--- bule.sh
+++ bule.sh
@@ -9,7 +9,7 @@ else
     if [ "$3" == "-g" ]
     then 
         echo -e "\n---------- Solver Output -----------\n"
-        ./main ground -t=1 -u=0 ../$2 2> /dev/null | grep -v '%% Rules:'
+        ./bule ground -t=1 -u=0 ../$2 2> /dev/null | grep -v '%% Rules:'
         echo -e "\n---------- Result Output -----------\n"
         ./bule $1 ../$2 2>/dev/null | grep "SAT\|UNSAT"
         echo ""
