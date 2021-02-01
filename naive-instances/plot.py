6afbcea62cd70c8186f2c3e8e73381e885ee8b7c
Sean <seanskg72@gmail.com>
2021-02-01 22:37:11 +1100

    naive instance graphing complete

--- naive-instances/plot.py
+++ naive-instances/plot.py
@@ -4,21 +4,22 @@ import sys
 import re
 
 # For naive encoding
-
+ct = 1
 x, y = [], []
 with open(sys.argv[1], "r") as file:
     for line in file:
         if re.search("SAT", line):
-            print(line, end="")
+            print(str(ct) + ". " + line, end="")
+            ct += 1
         elif re.search("real", line):
             y.append(re.search(".m.*s$", line).group(0))
         elif re.search("# Vertex", line):
-            num = int(re.search("# Vertex:.", line).group(0)[9:])
+            num = int(re.search("# Vertex:[0-9]*", line).group(0)[9:])
             x.append(num)
         else:
             print("Error")
 plt.scatter(x, y)
 plt.xlabel("Number of Vertices")
-plt.ylabel("Time")
+plt.ylabel("Real Time")
 plt.tight_layout()
 plt.savefig("naive-instances/naive_vertex.png")
